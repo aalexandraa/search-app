b241e458f11eaaed436d4347767ec8092c626220
aalexandraa <alex.proesl@gmail.com>
2017-08-15 18:52:30 +0200

    Add results to the bottom

--- src/App.js
+++ src/App.js
@@ -234,7 +234,35 @@ class App extends Component {
             </Button>
           </div>
         </div>
-
+        <br />
+        <div className='results'>
+          <table>
+            <tr>
+              <th>Bahnhöfe</th>
+              <th>Abfahrts-/Ankunftszeit</th>
+              <th>Gleis</th>
+              <th>Fahrtzeit</th>
+              <th>Umsteigen</th>
+              <th>Preis</th>
+            </tr>
+            <tr>
+              <td>Von: Berlin<br />Nach: Hamburg</td>
+              <td>Ab: 8:05Uhr<br />Bis: 10:15 Uhr</td>
+              <td>Gleis: 2<br /> Gleis: 19</td>
+              <td>2h10</td>
+              <td>0</td>
+              <td>29,90EUR</td>
+            </tr>
+            <tr>
+              <td>Von: Berlin<br />Nach: Hamburg</td>
+              <td>Ab: 9:30Uhr<br />Bis: 12:55 Uhr</td>
+              <td>Gleis: 1<br /> Gleis: 4</td>
+              <td>3h25</td>
+              <td>1</td>
+              <td>25,90EUR</td>
+            </tr>
+          </table>
+        </div>
       </div>
     )
   }
