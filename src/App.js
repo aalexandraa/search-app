ab8f965415986cf86ec689500a76cca22a0b4228
aalexandraa <alex.proesl@gmail.com>
2017-08-15 20:12:46 +0200

    Move autosuggest css into styled component wrapper

--- src/App.js
+++ src/App.js
@@ -17,6 +17,66 @@ const Title = styled.h1`
   color: palevioletred;
 `
 
+const StationSearch = styled.div`
+  .react-autosuggest__container {
+    position: relative;
+  }
+
+  .react-autosuggest__input {
+    width: 240px;
+    height: 30px;
+    padding: 10px 20px 10px 40px;
+    font-family: Helvetica, sans-serif;
+    font-weight: 300;
+    font-size: 16px;
+    border: 1px solid #aaa;
+    border-radius: 4px;
+  }
+
+  .react-autosuggest__input--focused {
+    outline: none;
+  }
+
+  .react-autosuggest__input--open {
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+
+  .react-autosuggest__suggestions-container {
+    display: none;
+  }
+
+  .react-autosuggest__suggestions-container--open {
+    display: block;
+    position: absolute;
+    top: 51px;
+    width: 300px;
+    border: 1px solid #aaa;
+    background-color: #fff;
+    font-family: Helvetica, sans-serif;
+    font-weight: 300;
+    font-size: 16px;
+    border-bottom-left-radius: 4px;
+    border-bottom-right-radius: 4px;
+    z-index: 2;
+  }
+
+  .react-autosuggest__suggestions-list {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+
+  .react-autosuggest__suggestion {
+    cursor: pointer;
+    padding: 10px 20px;
+  }
+
+  .react-autosuggest__suggestion--highlighted {
+    background-color: #ddd;
+  }
+`
+
 const Select = styled.select`
   background-color: white;
   font-size: 1em;
@@ -185,7 +245,7 @@ class App extends Component {
           <Title>Cheapest Train Finder App</Title>
         </div>
         <div className='Search'>
-          <div className='Stations'>
+          <StationSearch>
             <Autosuggest
               suggestions={originSuggestions}
               onSuggestionsFetchRequested={this.onOriginSuggestionsFetchRequested}
@@ -205,7 +265,7 @@ class App extends Component {
               inputProps={destinationInputProps}
               onSuggestionSelected={this.onDestinationSuggestionSelected}
             />
-          </div>
+          </StationSearch>
           <div className='Duration-picker'>
             <Select
               onChange={this.handleChangeDuration}
