30b8b8b5c19468ec2d3d7828661535f27ac47533
aalexandraa <alex.proesl@gmail.com>
2017-08-16 10:31:20 +0200

    Adding a thead and a tbody

--- src/Results.js
+++ src/Results.js
@@ -6,30 +6,34 @@ class Results extends Component {
     return (
       <div className='results'>
         <table>
-          <tr>
-            <th>Bahnhöfe</th>
-            <th>Abfahrts-/Ankunftszeit</th>
-            <th>Gleis</th>
-            <th>Fahrtzeit</th>
-            <th>Umsteigen</th>
-            <th>Preis</th>
-          </tr>
-          <tr>
-            <td>Von: Berlin<br />Nach: Hamburg</td>
-            <td>Ab: 8:05Uhr<br />Bis: 10:15 Uhr</td>
-            <td>Gleis: 2<br /> Gleis: 19</td>
-            <td>2h10</td>
-            <td>0</td>
-            <td>29,90EUR</td>
-          </tr>
-          <tr>
-            <td>Von: Berlin<br />Nach: Hamburg</td>
-            <td>Ab: 9:30Uhr<br />Bis: 12:55 Uhr</td>
-            <td>Gleis: 1<br /> Gleis: 4</td>
-            <td>3h25</td>
-            <td>1</td>
-            <td>25,90EUR</td>
-          </tr>
+          <thead>
+            <tr>
+              <th>Bahnhöfe</th>
+              <th>Abfahrts-/Ankunftszeit</th>
+              <th>Gleis</th>
+              <th>Fahrtzeit</th>
+              <th>Umsteigen</th>
+              <th>Preis</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Von: Berlin<br />Nach: Hamburg</td>
+              <td>Ab: 8:05Uhr<br />Bis: 10:15 Uhr</td>
+              <td>Gleis: 2<br /> Gleis: 19</td>
+              <td>2h10</td>
+              <td>0</td>
+              <td>29,90EUR</td>
+            </tr>
+            <tr>
+              <td>Von: Berlin<br />Nach: Hamburg</td>
+              <td>Ab: 9:30Uhr<br />Bis: 12:55 Uhr</td>
+              <td>Gleis: 1<br /> Gleis: 4</td>
+              <td>3h25</td>
+              <td>1</td>
+              <td>25,90EUR</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     )
