a7170b62c0fd9935421cf74444603845ed62c8ef
aalexandraa <alex.proesl@gmail.com>
2017-08-17 16:40:26 +0200

    Add styles to Results

--- src/Results.js
+++ src/Results.js
@@ -1,40 +1,65 @@
 import React, { Component } from 'react'
-// import styled from 'styled-components'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: left;
+    color: palevioletred;
+    margin-top: 20px;
+`
+
+const Th = styled.th`
+  font-size: 1em;
+  text-align: left;
+  padding: 5px;
+  color: palevioletred;
+`
+
+const Td = styled.td`
+  padding: 5px;
+  min-width: 100px;
+`
+
+const Table = styled.table`
+border: 1px solid #aaa;
+border-radius: 4px;
+`
 
 class Results extends Component {
   render () {
     return (
       <div className='results'>
-        <table>
+        <Title>Results:</Title>
+        <Table>
           <thead>
             <tr>
-              <th>Bahnhöfe</th>
-              <th>Abfahrts-/Ankunftszeit</th>
-              <th>Gleis</th>
-              <th>Fahrtzeit</th>
-              <th>Umsteigen</th>
-              <th>Preis</th>
+              <Th>Bahnhöfe</Th>
+              <Th>Abfahrts-/Ankunftszeit</Th>
+              <Th>Gleis</Th>
+              <Th>Fahrtzeit</Th>
+              <Th>Umsteigen</Th>
+              <Th>Preis</Th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>Von: Berlin<br />Nach: Hamburg</td>
-              <td>Ab: 8:05Uhr<br />Bis: 10:15 Uhr</td>
-              <td>Gleis: 2<br /> Gleis: 19</td>
-              <td>2h10</td>
-              <td>0</td>
-              <td>29,90EUR</td>
+              <Td>Von: Berlin<br />Nach: Hamburg</Td>
+              <Td>Ab: 8:05 Uhr<br />Bis: 10:15 Uhr</Td>
+              <Td>Gleis: 2<br /> Gleis: 19</Td>
+              <Td>2h10</Td>
+              <Td>0</Td>
+              <Td>29,90EUR</Td>
             </tr>
             <tr>
-              <td>Von: Berlin<br />Nach: Hamburg</td>
-              <td>Ab: 9:30Uhr<br />Bis: 12:55 Uhr</td>
-              <td>Gleis: 1<br /> Gleis: 4</td>
-              <td>3h25</td>
-              <td>1</td>
-              <td>25,90EUR</td>
+              <Td>Von: Berlin<br />Nach: Hamburg</Td>
+              <Td>Ab: 9:30 Uhr<br />Bis: 12:55 Uhr</Td>
+              <Td>Gleis: 1<br /> Gleis: 4</Td>
+              <Td>3h25</Td>
+              <Td>1</Td>
+              <Td>25,90 EUR</Td>
             </tr>
           </tbody>
-        </table>
+        </Table>
       </div>
     )
   }
