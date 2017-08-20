b9acbbb99c02b62fdab39e71bf51780b7bdc2308
aalexandraa <alex.proesl@gmail.com>
2017-08-20 16:53:03 +0200

    Install express

---
+++ bin/api.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var api = express();
+
+api.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+api.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
