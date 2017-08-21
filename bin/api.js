da0fee4f7ab433bc305c9a1f2e53d2ee93457a2a
aalexandraa <alex.proesl@gmail.com>
2017-08-21 21:55:18 +0200

    Respond with fake trips response

--- bin/api.js
+++ bin/api.js
@@ -6,7 +6,26 @@ app.use(cors())
 
 app.get('/trips', function (request, response) {
   response.json({
-    message: `Ohai there, I will soon return train journeys between ${request.query.origin} & ${request.query.destination} from ${request.query.start} to ${request.query.end} for ${request.query.duration} days. Just you wait.`
+    trips: [{
+      origin: 'Berlin',
+      destination: 'Hamburg',
+      departureTime: '8:05',
+      arrivalTime: '10:15',
+      departurePlatform: '2',
+      arrivalPlatform: '19',
+      legs: '1',
+      price: '29,90EUR'
+    },
+    {
+      origin: 'Berlin',
+      destination: 'Hamburg',
+      departureTime: '9:30',
+      arrivalTime: '12:55',
+      departurePlatform: '1',
+      arrivalPlatform: '4',
+      legs: '2',
+      price: '25,90EUR'
+    }]
   })
 })
 
