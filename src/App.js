c8ff2af6c6020f2279a4bb6137cd82c801963e4b
aalexandraa <alex.proesl@gmail.com>
2017-08-21 11:56:13 +0200

    Move StationSearch Styles into own file

--- src/App.js
+++ src/App.js
@@ -7,72 +7,13 @@ import { DateRangePicker } from 'react-dates'
 import moment from 'moment'
 import Results from './Results'
 import DatePickerStyles from './DatePickerStyles'
+import StationSearchStyles from './StationSearchStyles'
 
 // Styles
 const Title = styled.h1`
   font-size: 1.5em;
   text-align: left;
   color: palevioletred;
-`
-
-const StationSearch = styled.div`
-  .react-autosuggest__container {
-    position: relative;
-  }
-
-  .react-autosuggest__input {
-    width: 258px;
-    height: 30px;
-    padding: 10px 20px 10px 40px;
-    font-family: Helvetica, sans-serif;
-    font-weight: 300;
-    font-size: 16px;
-    border: 1px solid #aaa;
-    border-radius: 4px;
-  }
-
-  .react-autosuggest__input--focused {
-    outline: none;
-  }
-
-  .react-autosuggest__input--open {
-    border-bottom-left-radius: 0;
-    border-bottom-right-radius: 0;
-  }
-
-  .react-autosuggest__suggestions-container {
-    display: none;
-  }
-
-  .react-autosuggest__suggestions-container--open {
-    display: block;
-    position: absolute;
-    top: 51px;
-    width: 318px;
-    border: 1px solid #aaa;
-    background-color: #fff;
-    font-family: Helvetica, sans-serif;
-    font-weight: 300;
-    font-size: 16px;
-    border-bottom-left-radius: 4px;
-    border-bottom-right-radius: 4px;
-    z-index: 2;
-  }
-
-  .react-autosuggest__suggestions-list {
-    margin: 0;
-    padding: 0;
-    list-style-type: none;
-  }
-
-  .react-autosuggest__suggestion {
-    cursor: pointer;
-    padding: 10px 20px;
-  }
-
-  .react-autosuggest__suggestion--highlighted {
-    background-color: #ddd;
-  }
 `
 
 const Select = styled.select`
@@ -243,7 +184,7 @@ class App extends Component {
           <Title>Cheapest Train Finder App</Title>
         </div>
         <div className='Search'>
-          <StationSearch>
+          <StationSearchStyles>
             <Autosuggest
               suggestions={originSuggestions}
               onSuggestionsFetchRequested={this.onOriginSuggestionsFetchRequested}
@@ -263,7 +204,7 @@ class App extends Component {
               inputProps={destinationInputProps}
               onSuggestionSelected={this.onDestinationSuggestionSelected}
             />
-          </StationSearch>
+          </StationSearchStyles>
           <div className='Duration-picker'>
             <Select
               onChange={this.handleChangeDuration}
@@ -298,5 +239,6 @@ class App extends Component {
     )
   }
 }
+console.log('Hallo')
 
 export default App
